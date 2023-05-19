f741f6fde162963c7946857feedf4ccf912ba183
Ultimate_Deimos <egor.miloslavsky@yandex.ru>
2023-05-19 15:45:51 +0300

    Функция разности двух чисел

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def plus(num1, num2):
-    summ = num1 + num2
-    return summ
+    result = num1 + num2
+    return result
+
+
+def minus(num1, num2):
+    result = num1 + num2
+    return result
 
 if __name__ == '__main__':
     print(plus(1, 2))
+    print(minus(1, 2))
