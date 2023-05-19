6a3f8df88c35ed73fd8707cf972dbac605a1efa0
Ultimate_Deimos <egor.miloslavsky@yandex.ru>
2023-05-19 15:49:07 +0300

    bnmmgbm

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 def plus(num1, num2):
-    result = num1 + num2
-    return result
-
-
-def minus(num1, num2):
-    result = num1 + num2
-    return result
+    summ = num1 + num2
+    return summ
 
 if __name__ == '__main__':
     print(plus(1, 2))
-    print(minus(1, 2))
